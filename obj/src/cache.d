1020c5ec269cd892ab4ab9c26d958c803fbd45cc
Sujeet Chavhan <44285010+sujeetc@users.noreply.github.com>
2019-09-10 21:21:32 +0530

    Champsim Inclusive

---
+++ obj/src/cache.d
@@ -0,0 +1,25 @@
+obj/src/cache.o: src/cache.cc inc/cache.h inc/memory_class.h \
+ inc/champsim.h inc/block.h inc/instruction.h inc/set.h inc/set.h \
+ inc/uncore.h inc/cache.h inc/dram_controller.h inc/ooo_cpu.h
+
+inc/cache.h:
+
+inc/memory_class.h:
+
+inc/champsim.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/set.h:
+
+inc/set.h:
+
+inc/uncore.h:
+
+inc/cache.h:
+
+inc/dram_controller.h:
+
+inc/ooo_cpu.h:
